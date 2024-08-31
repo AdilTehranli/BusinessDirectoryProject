2de183461fbad90ab8bfe5f6efc24f365b58c15a
Adil Tehranli <adiljt@code.edu.az>
2024-08-31 17:45:27 +0300

    ScreenError

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,28 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+export default function _layout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+    <Tabs screenOptions={{headerShown:false}}>
+      <Tabs.Screen name='home'
+          options={{
+          tabBarLabel:'Profile',
+          tabBarIcon:({color})=> <FontAwesome name="home" size={24} color="black" />
         }}
       />
-      <Tabs.Screen
-        name="explore"
+      <Tabs.Screen name='explore'
+         options={{
+          tabBarLabel:'Profile',
+          tabBarIcon:({color})=> <FontAwesome name="home" size={24} color="black" />
+        }}
+      />
+      <Tabs.Screen name='profile' 
         options={{
-          title: 'Salam',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+          tabBarLabel:'Profile',
+          tabBarIcon:({color})=> <FontAwesome name="home" size={24} color="black" />
         }}
       />
     </Tabs>
-  );
+  )
 }
