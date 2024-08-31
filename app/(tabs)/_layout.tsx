557c39f4922f94c318807646df16dd471583f9e5
Adil Tehranli <adiljt@code.edu.az>
2024-08-31 18:39:15 +0300

    navigateErrorFixed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,25 +2,27 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 export default function _layout() {
   return (
     <Tabs screenOptions={{headerShown:false}}>
       <Tabs.Screen name='home'
           options={{
-          tabBarLabel:'Profile',
+          tabBarLabel:'Home',
           tabBarIcon:({color})=> <FontAwesome name="home" size={24} color="black" />
         }}
       />
       <Tabs.Screen name='explore'
          options={{
-          tabBarLabel:'Profile',
-          tabBarIcon:({color})=> <FontAwesome name="home" size={24} color="black" />
+          tabBarLabel:'Explore',
+          tabBarIcon:({color})=> <MaterialIcons name="explore" size={24} color="black" />
         }}
       />
       <Tabs.Screen name='profile' 
         options={{
           tabBarLabel:'Profile',
-          tabBarIcon:({color})=> <FontAwesome name="home" size={24} color="black" />
+          tabBarIcon:({color})=> <AntDesign name="profile" size={24} color="black" />
         }}
       />
     </Tabs>
