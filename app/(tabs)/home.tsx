59f141c058d068801531db1357a08f8374111f06
Adil Tehranli <adiljt@code.edu.az>
2024-09-03 13:48:48 +0300

    CreatedSlider

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header.jsx'
+import Slider from '../../components/Home/Slider.jsx'
 export default function home() {
   return (
     <View>
      <Header/>
+     <Slider/>
     </View>
   )
 }
---
+++ configs/FirebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYEM4gvCye7UWyCEIXHT_V6ucDDS9LnL0",
+  authDomain: "fir-education-251c0.firebaseapp.com",
+  projectId: "fir-education-251c0",
+  storageBucket: "fir-education-251c0.appspot.com",
+  messagingSenderId: "790918790276",
+  appId: "1:790918790276:web:7e952450a2bbf32d8216d1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
