a82e50baa72fd8f01e31c1e1b478a97f5da2f971
Adil Tehranli <adiljt@code.edu.az>
2024-09-03 17:47:19 +0300

    CreatedPopularBusiness

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,11 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header.jsx'
 import Slider from '../../components/Home/Slider.jsx'
+import Category from '../../components/Home/Category.jsx'
+import PopularBusiness from '../../components/Home/PopularBusiness.jsx'
 export default function home() {
   return (
     <View>
      <Header/>
      <Slider/>
+     <Category/>
+     <PopularBusiness/>
     </View>
   )
 }
--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Colors } from "@/constants/Colors";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "@/configs/FirebaseConfig";
+import CategoryItem from "../Home/CategoryItem";
 
 export default function Category() {
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    GetCategoryList();
+  }, []);
+
+  const GetCategoryList = async () => {
+    setCategoryList([]);
+    const q = query(collection(db, "Category"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      setCategoryList((prev) => [...prev, doc.data()]);
+    });
+  };
+
   return (
     <View>
-      <Text>Category</Text>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 20,
+          justifyContent: "space-between",
+          marginTop: 10,
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            fontFamily: "OutfitMedium",
+          }}
+        >
+          Category
+        </Text>
+        <Text style={{ color: Colors.PRIMARY, fontFamily: "OutfitMedium" }}>
+          View All
+        </Text>
+      </View>
+      <FlatList
+        horizontal={true}
+        data={categoryList}
+        showsHorizontalScrollIndicator={false}
+        style={{ marginLeft: 20 }}
+        renderItem={({ item, index }) => (
+          <CategoryItem category={item} key={index} onCategoryPress={(category)=>console.log(category)}/>
+        )}
+      />
     </View>
-  )
+  );
 }
