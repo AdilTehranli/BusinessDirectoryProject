cca4894aee610ce9fdcf87343218bf4bb2b03242
Adil Tehranli <adiljt@code.edu.az>
2024-09-05 11:12:31 +0300

    CreatedBusinessListByCategory

--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -4,9 +4,11 @@ import { Colors } from "@/constants/Colors";
 import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "@/configs/FirebaseConfig";
 import CategoryItem from "../Home/CategoryItem";
+import { useRouter } from "expo-router";
 
 export default function Category() {
   const [categoryList, setCategoryList] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     GetCategoryList();
@@ -52,7 +54,7 @@ export default function Category() {
         showsHorizontalScrollIndicator={false}
         style={{ marginLeft: 20 }}
         renderItem={({ item, index }) => (
-          <CategoryItem category={item} key={index} onCategoryPress={(category)=>console.log(category)}/>
+          <CategoryItem category={item} key={index} onCategoryPress={(category)=>router.push('businesslist/'+item.name)}/>
         )}
       />
     </View>
